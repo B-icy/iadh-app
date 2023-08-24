34c9e194d42b345f69646d5f7ff137684c0a0d9b
B-icy <40874650+B-icy@users.noreply.github.com>
2023-08-23 18:09:15 -0700

    Delete src directory

--- src/app/api/getUsers/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-export async function GET() {
-    const query = await prisma.Employee.findMany()
-    return NextResponse.json({ query })
-}
